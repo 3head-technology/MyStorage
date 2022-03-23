0cef16d3d69804468d7b3d31ad3c3588591caaa2
GBLenin <frosterart@gmail.com>
2022-03-23 19:28:59 +0100

    HomeView and UserView structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <base-button @click="toLogin" msg="Sign in"></base-button>
+  <div class="login-container">
+      <img src="../img/3head-logo.png" alt="3head-logo">
+      <h1>3Head Technologies</h1>
+      <base-button @click="toLogin" msg="Log In"></base-button>
   </div>
 </template>
 
@@ -18,4 +20,32 @@ export default {
 </script>
 
 <style scoped>
+    .login-container {
+        position: absolute;
+        font-family: 'Open Sans', sans-serif;
+        height: 25%;
+        width: 35%;
+        background-color: #fafafa;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        border-radius: 10px;
+        box-shadow: rgba(0, 0, 0, 0.24) 3px 3px 10px;
+        text-align: center;
+        margin: auto;
+        border: 1px solid #90B653;
+    }
+    .login-container img {
+        position: relative;
+        top: 5%;
+        width: 100px;
+    }
+    @media(max-width: 1200px) {
+      .login-container {
+            flex-direction: column;
+            justify-content: flex-start;
+            width: 648px;
+            height: 848px;
+        }
+    }
 </style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-      <keep-alive>
-      <input multiple @change="fileUploded" id="files" type="file">
-      </keep-alive>
+      <div class ="file-upload" @dragover.prevent @drop.prevent>
+            <input multiple @change="fileUploded" id="files" type="file">
+            <label for="files"></label>
+            <div @drop="dragFile" id ="drag-and-drop">
+                <div v-if="files.length">
+                    <ul v-for="file in files" :key="file">
+                        <li>{{file.name}}</li>
+                    </ul>
+                </div>
+            </div>
+            <p id="file-name"></p>
+      </div>
   </div>
 </template>
 
@@ -12,15 +21,63 @@ import BaseTextInput from "@/components/base/BaseTextInput";
 export default {
     name: "UserView",
     components: {BaseTextInput, },
+    data () {
+        return {
+            files: [],
+        };
+    },
+    computed: {
+        filesArray () {
+            return Array.from(this.files);
+        },
+    },
     methods: {
-        fileUploded () {
-            const files = document.getElementById("files").files;
-            console.log(files);
+        fileUploded (event) {
+            this.files = event.target.files;
+        },
+        dragFile (event) {
+            this.files = event.dataTransfer.files;
         },
     },
 };
+
 </script>
 
 <style scoped>
-
+    .file-upload {
+        position: absolute;
+        top: 25%;
+        left: 50%;
+        border-radius: 10px;
+    }
+    #files{
+        color: rgba(0, 0, 0, 0);
+    }
+    #files::-webkit-file-upload-button{
+        display: none;
+    }
+    #files::before{
+        content: 'Upload File';
+        display: inline-block;
+        border-radius: 10px;
+        padding: 7px 12px;
+        user-select: none;
+        font-weight: 500;
+        background-color: #90B653;
+        color: #fff;
+        text-decoration: none;
+        font-size: 1.2em;
+    }
+    #files:hover::before {
+        background-color: #aed373;
+        cursor: pointer;
+        transition: all ease-in-out 0.2s;
+    }
+    #drag-and-drop{
+        position: relative;
+        border-radius: 10px;
+        box-shadow: rgba(0, 0, 0, 0.24) 3px 3px 10px;
+        width: 200px;
+        height: 500px;
+    }
 </style>
